04e793642f492b3cb8cf6286fe3f41df0c42d27e
Omid K. Rad <omidkrad@users.noreply.github.com>
2021-10-09 18:22:05 -0700

    feat: use Fastify adapter

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter({
+      logger: true,
+    })
+  );
+  app.useGlobalPipes(new ValidationPipe());
+  await app.listen(3000, '0.0.0.0');
+  console.info(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
