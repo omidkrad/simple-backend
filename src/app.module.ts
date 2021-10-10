d2e1851c63d60bbec72cd1d7814bce335aa7b6c8
Omid K. Rad <omidkrad@users.noreply.github.com>
2021-10-09 21:12:11 -0700

    feat: add resources for product and review

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductModule } from './product/product.module';
+import { ReviewModule } from './review/review.module';
 
 @Module({
-  imports: [],
+  imports: [ProductModule, ReviewModule],
   controllers: [AppController],
   providers: [AppService],
 })
